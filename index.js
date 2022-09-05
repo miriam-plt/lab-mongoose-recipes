6048d5d29942e3ae71ad764fab503fe8eb4f4012
Miriam Poletti <polettimiriam@gmail.com>
2022-09-05 15:42:07 +0200

    done

--- index.js
+++ index.js
@@ -15,9 +15,39 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
+  
+  Recipe.create({
+    title: 'Spaghetti al pomodoro',
+    level: 'Easy Peasy',
+    ingredients: ['Spaghetti', 'Olive Oil', 'Garlic', 'Tomatoes', 'Salt', 'Papper', 'Sugar', 'Basil', 'Parmesan'],
+    cuisine: 'Italian',
+    image: 'https://www.giallozafferano.it/images/221-22163/Spaghetti-al-pomodoro_650x433_wm.jpg',
+    duration: 20
   })
+    .then(createdRecipe => console.log(createdRecipe.title))
+    .catch(err => console.log(err))
+
+
+   Recipe.insertMany(data)
+     .then((x)  => console.log(x))
+
+  })  
+  
+
+  Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100}, {new: true})
+    .then(() => console.log('successfully updated'))
+    .catch(err => console.log(err))
+ 
+
+  Recipe.findOneAndDelete({title: 'Carrot Cake'})
+    .then(() => console.log('successfully deleted'))
+    .catch(err => console.log(err))
+
+
   .catch(error => {
-    console.error('Error connecting to the database', error);
+      console.error('Error connecting to the database', error);
   });
